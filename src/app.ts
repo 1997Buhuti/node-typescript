6004fa728f4e0f1c7fd6c5a2f5bdfc4dfe115e23
Manakal <manakal@randomsoftware.net>
2022-06-13 06:35:57 +0530

    body parser added

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,11 @@ import mongoose, { ConnectOptions } from "mongoose";
 import Kitten from "./models/KittenClass";
 import "dotenv/config";
 import { getModelForClass } from "@typegoose/typegoose";
-const app = express();
 
+import bodyParser from "body-parser";
+const app = express();
+// parse application/json
+app.use(bodyParser.json());
 if (process.env.CONNECTION_STRING) {
   mongoose
     .connect(process.env.CONNECTION_STRING, {
@@ -24,24 +27,32 @@ app.get("/", (req: Request, res: Response): void => {
   res.json({ message: "Please Like the Video!" });
 });
 
-app.post(
-  "/addKittens",
-  async (req: Request, res: Response<Kitten>): Promise<void> => {
-    try {
-      const KittenModel = getModelForClass(Kitten);
-      const doc = new KittenModel({
-        name: res,
-        species: res,
-      });
-      await doc.save().then((value) => {
-        console.log(value);
-        res.send("Value inserted");
-      });
-    } catch (err) {
-      console.error(err);
-    }
+app.post("/addKittens", async (req: Request, res: Response) => {
+  console.log(req.body);
+  try {
+    const KittenModel = getModelForClass(Kitten);
+    const doc = new KittenModel({
+      name: req.body.name,
+      species: req.body.species,
+    });
+    await doc.save().then((value) => {
+      console.log(value);
+      res.send("Value inserted");
+    });
+  } catch (err) {
+    console.error(err);
   }
-);
+});
+
+app.get("/getKittens", async (req: Request, res: Response) => {
+  try {
+    const KittenModel = getModelForClass(Kitten);
+    const kittens = await KittenModel.find();
+    res.send(kittens);
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 app.listen("3001", (): void => {
   console.log("Server Running!");
